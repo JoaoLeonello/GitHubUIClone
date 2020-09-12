57184fd5666aaed1d12e77ebcbd8ecfd718d1134
João Leonello <jfelipe.pl@gmail.com>
2020-09-12 06:18:41 -0300

    :construction: Header add

--- github-clone/src/App.tsx
+++ github-clone/src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import GlobalStyles from './styles/GlobalStyles';
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
+
 function App() {
     return 
     <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<Profile />} />
-        <Route path="/:username" element={<Profile />} />
-        <Route path="/:username/:reponame" element={<Profile />} />
-    </Routes>
+      <Header />
+        <Routes>
+            <Route path="/" element={<Profile />} />
+            <Route path="/:username" element={<Profile />} />
+            <Route path="/:username/:reponame" element={<Profile />} />
+        </Routes>
+
+        <GlobalStyles />
     </BrowserRouter>
 }
 
