b634ef41ded6c01df04287ee4c3aa5f66ecac33b
João Leonello <jfelipe.pl@gmail.com>
2020-09-12 20:05:50 -0300

    :construction: User info

--- github-clone/src/pages/Profile/index.tsx
+++ github-clone/src/pages/Profile/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide } from './styles';
+
+import ProfileData from '../../components/ProfileData';
 
 const Profile: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Main>
+        <LeftSide></LeftSide>
+          <ProfileData
+            username={'JoaoLeonello'}
+            name={'Joao Leonello'}
+            avatarUrl={'https://avatars0.githubusercontent.com/u/55457001?s=400&u=911875996ddb2654dfdba7cebbe4dfee71e3b4b8&v=4'}
+            followers={887}
+            following={7}
+            company={'Rocketseat'}
+            location={'Londrina, Brazil'}
+            email={'jfelipe.pl@gmail.com'}
+            blog={undefined}
+          />
+        <RightSide></RightSide>
+      </Main>
+    </Container>
+  )
 }
 
 export default Profile;
